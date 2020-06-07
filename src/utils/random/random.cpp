fad4b6700878d74e6a60646b42bc3a67bfaa98b6
JAN VOJACEK <vojacekjan@hotmail.com>
2020-06-07 09:36:34 +0200

    Renames internal values in random number generation function for better clarity, comment improved

--- src/utils/random/random.cpp
+++ src/utils/random/random.cpp
@@ -5,16 +5,16 @@
 std::function<int()> random_utils::get_int_function(const int& max_value) {
     // Random engine & distribution must be allocated on a heap memory in order to be preserved
     // Shared pointer ensures memory release once lambda gets deleted
-    auto random_engine = std::make_shared<std::default_random_engine>(time(nullptr));
-    auto distribution = std::make_shared<std::uniform_int_distribution<>>(1, max_value);
+    auto random_engine_ptr = std::make_shared<std::default_random_engine>(time(nullptr));
+    auto distribution_ptr = std::make_shared<std::uniform_int_distribution<>>(1, max_value);
 
-    // Pointers must be dereference in order to be used as values
-    auto r = *random_engine;
-    auto d = *distribution;
+    // Dereference pointers in order to be used as values
+    auto random_engine = *random_engine_ptr;
+    auto distribution = *distribution_ptr;
 
     // Lambda must contain "mutable" keyword in order to be able to mutate the state of the distribution
     // after each invocation
-    return [r, d]() mutable {
-        return d(r);
+    return [random_engine, distribution]() mutable {
+        return distribution(random_engine);
     };
 }
